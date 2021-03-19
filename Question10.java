cbbcfe64af9d8372be3634a2ea00a7ac2b56a5d1
BerkerToklac <80786295+BerkerToklac@users.noreply.github.com>
2021-03-19 15:51:51 +0300

    Add files via upload

---
+++ Question10.java
@@ -0,0 +1,16 @@
+package day3_varaibles_dataTypes;
+
+public class Question10 {
+
+	public static void main(String[] args) {
+		
+		int hh= 13;
+		int mm= 50;
+		int ss=35;
+		
+		System.out.println(""+hh+ ":" +mm +":"+ss + " AM" );
+
+
+	}
+
+}
